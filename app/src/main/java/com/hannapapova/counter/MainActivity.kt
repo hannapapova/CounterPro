140aaeb02e5bc106947a9df296054c6e8ae8123a
Hanna Papova <gannapapova@gmail.com>
2021-04-07 13:10:31 +0300

    Add Firebase Analytics and Crashlytics

--- app/src/main/java/com/hannapapova/counter/MainActivity.kt
+++ app/src/main/java/com/hannapapova/counter/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.hannapapova.counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NullPointerException
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
+    private val id = "counter-25fff"
+    private val name = "Counter"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id)
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, name)
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        mFirebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+
+        btn_crash.setOnClickListener {
+            throw NullPointerException("Null Pointer Crash!")
+        }
     }
 }
