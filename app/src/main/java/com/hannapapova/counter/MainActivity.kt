39e3130bd5af79feda0cdb5c3a50979e1d336039
Hanna Papova <gannapapova@gmail.com>
2021-04-09 12:51:45 +0300

    Add splash screen and chang icon color

--- app/src/main/java/com/hannapapova/counter/MainActivity.kt
+++ app/src/main/java/com/hannapapova/counter/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Counter)
         setContentView(R.layout.activity_main)
 
         setupFirebase()
